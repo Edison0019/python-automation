ba4b26c1190953eb16bbe6ad6d20c9a897033dbe
edison0019 <edison_0019@hotmail.com>
2020-05-02 20:40:32 -0400

    new zip code distance calculator included

---
+++ zipcode_distance/zipcode_distance.py
@@ -0,0 +1,48 @@
+#this code is for getting the distance of a list of zip codes for a given zip code
+#it returns a file with the elementes of the list as distance in KM
+from uszipcode import SearchEngine
+
+#getting the distance of two pair of coordinates
+def coordkm(lat1,lon1,lat2,lon2):
+    import math
+    if lat2 == None or lon2 == None:
+        return 'None'
+    R = 6373.0
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    distance = R * c
+    return distance
+
+def distance(reference,file):
+    with open(file,'r') as f:
+        dirtyzip = f.readlines()
+        cleanzip = []
+        for i in dirtyzip:
+            cleanzip.append(int(i.replace('\n','')))
+
+    #get latitude and longitud for each zip code and the reference zip code
+    search = SearchEngine(simple_zipcode=True)
+    zlatlog = {}
+    for z in cleanzip:
+        x = search.by_zipcode(int(z))
+        zlatlog[z] = (x.lat,x.lng)
+
+    #get a list of distances
+    refzip = search.by_zipcode(reference)
+    kmdist = []
+    for i in zlatlog:
+        d2 = (zlatlog[i][0],zlatlog[i][1])
+        kmdist.append(coordkm(refzip.lat,refzip.lng,zlatlog[i][0],zlatlog[i][1]))
+    
+    #write the contents to a file
+    with open(str(reference)+'.txt','w') as f:
+        for i in kmdist:
+            f.write(str(i)+'\n')
+
+try:
+    for i in [19403,19153]:
+        distance(i,'zip.txt')
+except EnvironmentError as e:
+    print(e)
